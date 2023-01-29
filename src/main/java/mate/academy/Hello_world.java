dfe3b1bfcd9576e8d143bebf19816089b11847ac
Alona Klishchenko <alyonaklischenko@gmail.com>
2023-01-29 14:20:55 +0200

    Corrected a typo

--- src/main/java/mate/academy/Hello_world.java
+++ src/main/java/mate/academy/Hello_world.java
@@ -1,7 +1,7 @@
 package mate.academy;
 
-public class Hello_world {
-    public static void main(String args) {
+class Hello_world {
+    public static void main(String[] args) {
         System.out.println("Hello, mates!");
     }
 }
